c12d0531c6452d6501cf1f541694c042ee397c08
rr <jhu>
2024-01-22 17:15:18 +0545

    created 2 route.js and imported in this file

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const userRouter = require("./user.router");
+const adminRouter = require("./admin.router");
+
 // Printing Hello World!
 router.get("/", (request, response) => {
   response.json({ msg: "Hello World!" });
 });
 
-router.get("/:name/:welcome", (request, response) => {
-  const data = request.params.name;
-  const data2 = request.params.welcome;
-  response.json({ msg: `Hello ${data}! Welcome to ${data2} website` });
-});
-
-router.get("/sum/:number1/:number2", (req, res) => {
-  const { number1, number2 } = req.params;
-  const sum = parseInt(number1) + parseInt(number2);
-  res.json({ msg: `The sum is ${sum}` });
-});
-
+router.use("/user", userRouter);
+router.use("/admin", adminRouter);
 module.exports = router;
