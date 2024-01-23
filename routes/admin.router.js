ca660da0590f778933f9c4b38fa57483cf71d5a8
rr <jhu>
2024-01-23 18:15:02 +0545

    used post method

--- routes/admin.router.js
+++ routes/admin.router.js
@@ -3,4 +3,12 @@ const router = require("express").Router();
 router.get("/", (req, res) => {
   res.json({ data: `hello from admin` });
 });
+
+router.post("/", (req, res) => {
+  //req.params => req.params
+  //req.query => ?key=value
+  //req.body => raw json ({"key": "value"})
+  console.log(req.query, req.body);
+});
+
 module.exports = router;
