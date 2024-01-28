5562c7194960c787f00bd8ee81f737056fa33291
rr <ughrahul@users.noreply.github.com>
2024-01-28 17:35:50 +0545

    used parameters in middlewares

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,16 +1,42 @@
 const router = require("express").Router(); //chaining of methods
 const { numberFormatter } = require("../utils/numberFormatter");
 
-router.get("/", (req, res) => {
+// router.get("/", (req, res, next) => {
+//   try {
+//     res.json({ data: `hello form user` });
+//   } catch (e) {
+//     next(e);
+//   }
+// });
+
+//
+const checkRole = (req, res, next) => {
+  console.log(req.headers);
+  console.log("Hello, I'm Role checker");
+  next();
+};
+
+const secondRoler = (req, res, next) => {
+  console.log(req.body);
+  console.log("Hello, I'm  Second Roler");
+  next();
+};
+
+router.get("/", checkRole, secondRoler, (req, res, next) => {
   try {
-    res.json({ data: `hello form user` });
+    console.log(req.body);
+    res.json({ msg: "hello from user route" });
   } catch (e) {
     next(e);
   }
 });
 
-router.post("/:number", (req, res, next) => {
+router.post("/", (req, res, next) => {
   try {
+    const data = Object.keys(req.body).length;
+    if (!data) throw new Error("Body missing");
+    console.log({ data: req.body });
+
     const result = numberFormatter(
       req.params.number || req.body.number || req.query.number
     );
@@ -20,14 +46,35 @@ router.post("/:number", (req, res, next) => {
   }
 });
 
-router.put("/", (req, res) => {
-  console.log(req.query, req.body);
+router.put("/:id", (req, res, next) => {
+  try {
+    const result = numberFormatter(
+      req.params.number || req.body.number || req.query.number
+    );
+    res.json({ result });
+  } catch (e) {
+    next(e);
+  }
 });
-router.patch("/", (req, res) => {
-  console.log(req.query, req.body);
+router.patch("/:id", (req, res, next) => {
+  try {
+    const result = numberFormatter(
+      req.params.number || req.body.number || req.query.number
+    );
+    res.json({ result });
+  } catch (e) {
+    next(e);
+  }
 });
-router.delete("/", (req, res) => {
-  console.log(req.query, req.body);
+router.delete("/:id", (req, res, next) => {
+  try {
+    const result = numberFormatter(
+      req.params.number || req.body.number || req.query.number
+    );
+    res.json({ result });
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = router;
