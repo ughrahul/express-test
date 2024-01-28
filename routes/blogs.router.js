a031a9511729a65435559b566d07c6ab1053fcc2
rr <ughrahul@users.noreply.github.com>
2024-01-28 17:34:29 +0545

    blog routes

--- routes/blogs.router.js
+++ routes/blogs.router.js
@@ -4,8 +4,15 @@ router.get("/", (req, res) => {
   res.json({ data: `hello form blogs` });
 });
 
-router.post("/", (req, res) => {
-  console.log(req.query, req.body);
+router.post("/:id", (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
+    if (!id || !name) throw "ID and Name is missing";
+    res.json({ msg: `Hello from blog id ${id} of blog name ${name} ` });
+  } catch (e) {
+    next(e);
+  }
 });
 router.put("/", (req, res) => {
   console.log(req.query, req.body);
