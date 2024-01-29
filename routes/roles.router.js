0c69fa894ca966a84c68652f0425773760d4d2c4
rr <ughrahul@users.noreply.github.com>
2024-01-30 01:00:04 +0545

    checked roles and encrypted password

--- routes/roles.router.js
+++ routes/roles.router.js
@@ -1,18 +1,53 @@
-const router = require("express").Router(); //chaining of methods
+const express = require("express");
+const bcrypt = require("bcrypt");
+const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ data: `hello form roles` });
+router.get("/", (req, res, next) => {
+  res.json({ msg: "Welcome to roles route" });
+  next();
 });
-router.post("/", (req, res) => {
-  console.log(req.query, req.body);
+
+const checkRoles = (sysRole) => {
+  return (req, res, next) => {
+    const userRoles = req.body.role
+      ? [req.body.role]
+      : req.headers.role
+      ? [req.headers.role]
+      : [];
+    console.log(userRoles);
+    if (userRoles.length === 0) throw new Error("Roles are missing");
+    const result = userRoles.some((role) => sysRole.includes(role));
+    if (!result) throw new Error("Permission denied");
+    next();
+  };
+};
+
+const encryptPw = async (req, res, next) => {
+  try {
+    if (req.body.password) {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      req.body.password = hashedPassword;
+      res.json({ Bcrypted_Password: req.body.password });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+router.get("/user", checkRoles(["user"]), encryptPw, (req, res, next) => {
+  try {
+    res.json({ msg: "Welcome User!" });
+  } catch (e) {
+    next(e);
+  }
 });
-router.put("/", (req, res) => {
-  console.log(req.query, req.body);
+
+router.get("/admin", checkRoles(["admin"]), encryptPw, (req, res, next) => {
+  try {
+    res.json({ msg: "Welcome Admin!" });
+  } catch (e) {
+    next(e);
+  }
 });
-router.patch("/", (req, res) => {
-  console.log(req.query, req.body);
-});
-router.delete("/", (req, res) => {
-  console.log(req.query, req.body);
-});
+
 module.exports = router;
