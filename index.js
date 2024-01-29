c29b75fdc4be604b6479cbd2ee9c7adfe20c5feb
rr <ughrahul@users.noreply.github.com>
2024-01-30 00:58:53 +0545

    added url in listen

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(8000, () => {
-  console.log("Application is running...");
+  console.log("Application is running on port http://localhost:8000");
 });
