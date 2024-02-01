1b9ee9ab45ea53965399f2089a9e36fac0547795
rr <ughrahul@users.noreply.github.com>
2024-02-01 17:39:06 +0545

    added blogRouter for database

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
-
+const mongoose = require("mongoose");
 const indexRouter = require("./routes");
+const blogRouter = require("./modules/blog.route");
 
-//effortlessly handle incoming requests containing JSON data
-//by automatically parsing and making it accessible in the program.(middleware)
+//connecting to database
+mongoose.connect("mongodb://localhost:27017/rahulapp").then(() => {
+  console.log("Database is Connected.");
+});
+
+//handles incoming requests containing JSON data
 app.use(express.json());
 
 //using global middleware which takes request first to show the date
@@ -14,15 +19,21 @@ app.use((req, res, next) => {
   next();
 });
 
+//using to show static files from public directory.
 app.use(express.static("public"));
+
+//using blogs
+app.use("/", blogRouter);
 
 app.use("/", indexRouter);
 
+//handles error
 app.use((err, req, res, next) => {
   err = err ? err.toString() : "Something went wrong";
   res.status(500).json({ msg: err });
 });
 
+//listens app on p=given port
 app.listen(8000, () => {
   console.log("Application is running on port http://localhost:8000");
 });
