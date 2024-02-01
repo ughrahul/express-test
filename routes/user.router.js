0c6804e9dccda1a8889b43d2bcd823b928f99fd0
rr <ughrahul@users.noreply.github.com>
2024-02-01 17:41:07 +0545

    user router

--- routes/user.router.js
+++ routes/user.router.js
@@ -9,69 +9,79 @@ const { numberFormatter } = require("../utils/numberFormatter");
 //   }
 // });
 
-//
-const checkRole = (req, res, next) => {
-  console.log(req.headers);
-  console.log("Hello, I'm Role checker");
-  next();
-};
+// //
+// const checkRole = (req, res, next) => {
+//   console.log(req.headers);
+//   console.log("Hello, I'm Role checker");
+//   next();
+// };
 
-const secondRoler = (req, res, next) => {
-  console.log(req.body);
-  console.log("Hello, I'm  Second Roler");
-  next();
-};
+// const secondRoler = (req, res, next) => {
+//   console.log(req.body);
+//   console.log("Hello, I'm  Second Roler");
+//   next();
+// };
 
-router.get("/", checkRole, secondRoler, (req, res, next) => {
-  try {
-    console.log(req.body);
-    res.json({ msg: "hello from user route" });
-  } catch (e) {
-    next(e);
-  }
-});
+// router.get("/", checkRole, secondRoler, (req, res, next) => {
+//   try {
+//     console.log(req.body);
+//     res.json({ msg: "hello from user route" });
+//   } catch (e) {
+//     next(e);
+//   }
+// });
+
+// router.post("/", (req, res, next) => {
+//   try {
+//     const data = Object.keys(req.body).length;
+//     if (!data) throw new Error("Body missing");
+//     console.log({ data: req.body });
+
+//     const result = numberFormatter(
+//       req.params.number || req.body.number || req.query.number
+//     );
+//     res.json({ result });
+//   } catch (e) {
+//     next(e);
+//   }
+// });
+
+// router.put("/:id", (req, res, next) => {
+//   try {
+//     const result = numberFormatter(
+//       req.params.number || req.body.number || req.query.number
+//     );
+//     res.json({ result });
+//   } catch (e) {
+//     next(e);
+//   }
+// });
+// router.patch("/:id", (req, res, next) => {
+//   try {
+//     const result = numberFormatter(
+//       req.params.number || req.body.number || req.query.number
+//     );
+//     res.json({ result });
+//   } catch (e) {
+//     next(e);
+//   }
+// });
+// router.delete("/:id", (req, res, next) => {
+//   try {
+//     const result = numberFormatter(
+//       req.params.number || req.body.number || req.query.number
+//     );
+//     res.json({ result });
+//   } catch (e) {
+//     next(e);
+//   }
+// });
 
 router.post("/", (req, res, next) => {
   try {
-    const data = Object.keys(req.body).length;
-    if (!data) throw new Error("Body missing");
-    console.log({ data: req.body });
-
-    const result = numberFormatter(
-      req.params.number || req.body.number || req.query.number
-    );
-    res.json({ result });
-  } catch (e) {
-    next(e);
-  }
-});
-
-router.put("/:id", (req, res, next) => {
-  try {
-    const result = numberFormatter(
-      req.params.number || req.body.number || req.query.number
-    );
-    res.json({ result });
-  } catch (e) {
-    next(e);
-  }
-});
-router.patch("/:id", (req, res, next) => {
-  try {
-    const result = numberFormatter(
-      req.params.number || req.body.number || req.query.number
-    );
-    res.json({ result });
-  } catch (e) {
-    next(e);
-  }
-});
-router.delete("/:id", (req, res, next) => {
-  try {
-    const result = numberFormatter(
-      req.params.number || req.body.number || req.query.number
-    );
-    res.json({ result });
+    const data = req.body;
+    console.log({ data });
+    res.json({ msg: "Hello from user route" });
   } catch (e) {
     next(e);
   }
